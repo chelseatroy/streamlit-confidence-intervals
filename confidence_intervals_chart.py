7be73f46c86033002dc77cd0c662f06dbda02629
Chelsea Troy <chelsea.dommert@gmail.com>
2021-10-08 21:22:42 -0500

    Add more explanatory text for folks to explore on their own

--- confidence_intervals_chart.py
+++ confidence_intervals_chart.py
@@ -11,11 +11,11 @@ st.title('Confidence Intervals Visualized on Chicago Maintenance Data')
 def load_data():
     data = pd.read_csv('metrics.csv')
     aggregation = data \
-        .assign(average_days_to_complete_activity=lambda row: row["Average Days to Complete Activity"].apply(lambda x: float(x))) \
+        .assign(mean_days_to_complete_activity=lambda row: row["Average Days to Complete Activity"].apply(lambda x: float(x))) \
         .groupby('Activity') \
         .agg({
              'Target Response Days': 'max',
-             'average_days_to_complete_activity': ['mean','std'],
+             'mean_days_to_complete_activity': ['mean','std'],
              'Activity' : 'count'
             })\
         .reset_index()
@@ -27,14 +27,22 @@ def update_metrics(aggregation, columns, data_amount=60.0, confidence=95.0):
     conf_interval_bottom, conf_interval_top = \
         confidence_interval_for_collection(
             sample_size=num_examples,
-            standard_deviation=aggregation["average_days_to_complete_activity std"],
-            mean=aggregation["average_days_to_complete_activity mean"],
+            standard_deviation=aggregation["mean_days_to_complete_activity std"],
+            mean=aggregation["mean_days_to_complete_activity mean"],
             confidence=confidence / 100
         )
     return num_examples, conf_interval_bottom, conf_interval_top
 
 aggregation = load_data()
 aggregation
+
+"We'll look at the mean amount of time that each of these types of projects took in days."
+
+"However, we want to do that responsibly. And an mean means a lot less when it's based on just a FEW data points than it does when it's based on MANY data points."
+
+"We can account for this with a confidence interval, which sort of represents the error bars around an mean. Less data, larger error bars. More data, smaller ones—because we're more confident that the mean of a lot of data represents a population mean."
+
+"This chart shows the mean project length for each type of project, with a confidence interval around it."
 
 amount_of_data = st.slider('Percentage of data to use', 0, 100, 60)
 confidence = st.slider('Probability of the true mean falling between the red dots', 0, 100, 95)
@@ -44,7 +52,7 @@ num_examples, conf_interval_bottom, conf_interval_top = update_metrics(aggregati
 fig = plt.figure(figsize=(10,14))
 ax = fig.add_subplot(1,1,1)
 ax.scatter(
-    aggregation["average_days_to_complete_activity mean"],
+    aggregation["mean_days_to_complete_activity mean"],
     aggregation["Activity"],
     c='b'
 )
@@ -61,3 +69,5 @@ ax.scatter(
 plt.xticks(rotation=88)
 
 st.write(fig)
+
+"Based on this plot, which projects do you think have only a few data points? Which ones have many?"
